64b297ea243ead8597257d99fc26802a53d4e637
Todd Mette <toddmette@gmail.com>
2018-11-13 16:29:54 -0600

    added more locations

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,24 +3,29 @@ var locationName = document.querySelector(".location-name");
 
 function getLocation() {
 	var locationArray = [
+		"Anvil",
 		"Array",
 		"Asylum",
+		"Boxing Gym",
 		"Cargo Docks",
 		"Construction Site",
+		"Diner",
 		"Estates",
 		"Factory",
 		"Firing Range",
 		"Fracking Tower",
+		"Graveyard",
 		"Hydro Dam",
 		"Lighthouse",
 		"Nuketown Island",
+		"Pavilion",
 		"Rivertown",
+		"The 'Burbs (six houses)",
 		"Train Station",
 		"Turbine"
 	];
 	var randomLocation = locationArray[Math.floor(Math.random() * locationArray.length)];
 	locationName.innerHTML=randomLocation;
-	// console.log("clicked!");
 }
 
 
