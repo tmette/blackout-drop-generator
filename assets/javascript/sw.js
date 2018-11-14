df667b8a4560d033fc9647d487eddd086d27de3d
Todd Mette <toddmette@gmail.com>
2018-11-14 10:09:18 -0600

    updated service worker

--- assets/javascript/sw.js
+++ assets/javascript/sw.js
@@ -1,18 +1,43 @@
+var cacheName = 'drop-gen-pwa-v1';
+var appShellFiles = [
+    '/',
+    '../../index.html',
+    '/index.js',
+    '../css/main.css',
+    '../images/icons/icon-72x72.png',
+    '../images/icons/icon-96x96.png',
+    '../images/icons/icon-128x128.png',
+    '../images/icons/icon-144x144.png',
+    '../images/icons/icon-152.152.png',
+    '../images/icons/icon-192.192.png',
+    '../images/icons/icon-384x384.png',
+    '../images/icons/icon-512x512.png'
+];
+var contentToCache = appShellFiles;
+
+
 self.addEventListener('install', function(e) {
     console.log('[Service Worker] Install');
-    var cacheName = 'drop-gen-pwa-v1';
-    var appShellFiles = [
-        '/',
-        '../../index.html',
-        '/index.js',
-        '../css/main.css',
-        '../images/icons/icon-72x72.png',
-        '../images/icons/icon-96x96.png',
-        '../images/icons/icon-128x128.png',
-        '../images/icons/icon-144x144.png',
-        '../images/icons/icon-152.152.png',
-        '../images/icons/icon-192.192.png',
-        '../images/icons/icon-384x384.png',
-        '../images/icons/icon-512x512.png'
-    ];
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(contentToCache);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(r) {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+            return r || fetch(e.request).then(function(response) {
+                return caches.open(cacheName).then(function(cache) {
+                    console.log('[Service Worker] Caching new resource: '+e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
 });
