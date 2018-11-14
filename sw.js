5472f9a99aa7304ac6d78651a3f73decb9bb807a
Todd Mette <toddmette@gmail.com>
2018-11-14 12:11:47 -0600

    updated file paths

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 var cacheName = 'drop-gen-pwa-v1';
 var appShellFiles = [
-    '/',
-    '/index.html',
-    '/assets/javascript/index.js',
-    '/assets/css/main.css',
-    '/assets/images/icons/icon-72x72.png',
-    '/assets/images/icons/icon-96x96.png',
-    '/assets/images/icons/icon-128x128.png',
-    '/assets/images/icons/icon-144x144.png',
-    '/assets/images/icons/icon-152x152.png',
-    '/assets/images/icons/icon-192x192.png',
-    '/assets/images/icons/icon-384x384.png',
-    '/assets/images/icons/icon-512x512.png'
+    './',
+    './index.html',
+    './assets/javascript/index.js',
+    './assets/css/main.css',
+    './assets/images/icons/icon-72x72.png',
+    './assets/images/icons/icon-96x96.png',
+    './assets/images/icons/icon-128x128.png',
+    './assets/images/icons/icon-144x144.png',
+    './assets/images/icons/icon-152x152.png',
+    './assets/images/icons/icon-192x192.png',
+    './assets/images/icons/icon-384x384.png',
+    './assets/images/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', function(e) {
